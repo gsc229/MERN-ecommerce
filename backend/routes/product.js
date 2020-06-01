a306054607711544cf51696301a9f6cfd7bb045e
greg <gsc229@gmail.com>
2020-06-01 16:00:43 -0400

    product CRUD functional

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-const {create} = require('../controllers/product')
+const {create, productById, read, remove, update} = require('../controllers/product')
 const {requireSignIn, isAdmin, isAuth} = require('../controllers/auth')
 const {userById} = require('../controllers/user')
 
 router.post('/products/create/:userId', requireSignIn,isAuth, isAdmin, create)
-
+router.get('/products/:productId', requireSignIn, read)
+router.delete('/products/:productId/:userId', requireSignIn,isAuth, isAdmin, remove)
+router.put('/products/:productId/:userId', requireSignIn,isAuth, isAdmin, update)
 
 router.param('userId', userById)
+router.param('productId', productById)
 
 module.exports = router
