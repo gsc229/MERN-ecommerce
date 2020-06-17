12f4235580d49ad99dec9445fa64d1a2ac5c91dc
greg <gsc229@gmail.com>
2020-06-16 21:25:25 -0400

    created method to create a new procuct

--- frontend/ecommerce/src/utils/axiosWithAuth.js
+++ frontend/ecommerce/src/utils/axiosWithAuth.js
@@ -3,12 +3,11 @@ import {API} from '../config'
 const axiosWithAuth = () => {
   const jwt = JSON.parse(localStorage.getItem('jwt'));
   
-  console.log('BASE URL: ', API)
+  
   return axios.create({
     baseURL: API,
     headers: {
-      Authorization: jwt ? `Bearer ${jwt.token}` : '',
-      'Content-Type': 'application/json',
+      Authorization: jwt ? `Bearer ${jwt.token}` : '',    
       Accept: 'application/json'
     },
     
