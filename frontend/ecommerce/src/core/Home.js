432117a286989df071c8214ab33e9f4a9c1b6214
greg <gsc229@gmail.com>
2020-06-17 13:56:27 -0400

    created an apiCore file to hold core app api calls. Getting product data sorted by arrival and sales in Home

--- frontend/ecommerce/src/core/Home.js
+++ frontend/ecommerce/src/core/Home.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Layout from './Layout'
+import {getProducts} from './apiCore'
+
 
 const Home = () => {
+
+  const [productsBySell, setProductsBySell] = useState([])
+  const [productsByArrival, setProductsByArrival] = useState([])
+  const [error, setError] = useState([])
+
+
+  const loadProductsBySell = ()=>{
+    getProducts('sold')
+    .then(data=>{
+      if(data.error){
+        setError(data.error)
+      } else{
+        setProductsBySell(data)
+      }
+    })
+  }
+
+  const loadProductsByArrival = ()=>{
+    getProducts('createdAt')
+    .then(data=>{
+      if(data.error){
+        setError(data.error)
+      } else{
+        setProductsByArrival(data)
+      }
+    })
+  }
+
+
+  useEffect(()=>{
+    loadProductsByArrival()
+    loadProductsBySell()
+  },[])
+
   return (
     <Layout title="Home Page" description="Node React E-commerce App">
-      This is...
+      {JSON.stringify(productsBySell)}
+      <hr/>
+      
+      <h1>By Arrival</h1>
+      {JSON.stringify(productsByArrival)}
     </Layout>
      
   )
